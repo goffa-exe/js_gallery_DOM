b4279b8e442bc630ef4fa15c27d4c41ca54dfcad
gOFFa <n4v4rm0re666@gmail.com>
2025-08-16 19:55:58 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const largeImg = document.querySelector('.gallery__large-img');
+
+for (const link of document.querySelectorAll('.list-item__link')) {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    largeImg.setAttribute('src', link.href);
+  });
+}
